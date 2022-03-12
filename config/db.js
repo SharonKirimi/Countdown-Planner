d565ee6b1284f1528daf1a8a21053ae55d4bfea3
Sharon Kirimi <kirimisharon@gmail.com>
2022-03-12 16:26:09 +0100

    Backend tests - working now

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'Countdown Planner'
+const mongooseBaseName = 'countdown-planner'
 
 // create the mongodb uri for development and test
 const database = {
-  development: `mongodb://localhost/${mongooseBaseName}-development`,
-  test: `mongodb://localhost/${mongooseBaseName}-test`
+  development: `mongodb://127.0.0.1/${mongooseBaseName}-development`,
+  test: `mongodb://127.0.0.1/${mongooseBaseName}-test`
 }
 
 // Identify if development environment is test or development
